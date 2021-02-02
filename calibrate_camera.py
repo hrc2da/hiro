0206f89af7b67948c9bd37ea9e38e450ccda9df2
dmd354 <dmd354@cornell.edu>
2021-02-02 15:38:43 -0500

    added more heights to calibration script

--- calibrate_camera.py
+++ calibrate_camera.py
@@ -8,10 +8,11 @@ from hiro_interface import *
 
 hiro = HIRO()
 
-heights = [100, 150, 200, 250]
+heights = [80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250]
 
 for z in heights:
     hiro.move(np.array([[0], [200], [z]]))
+    #print(z)
     hiro.capture('/home/pi/Desktop/calibration/%d.jpg' % z)
 
 hiro.disconnect()
